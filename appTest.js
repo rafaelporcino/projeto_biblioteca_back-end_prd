d4757bd393df2014df1f69f2ee1f8354cdbcd435
rafaelporcino <rafaelporcino@gmail.com>
2022-11-22 19:20:35 -0300

    Atualização

--- appTest.js
+++ appTest.js
@@ -1,40 +1,40 @@
 //Main
-let cadastroProdutos = require('./persistence/emprestimo_persistence')
+let cadastroEmprestimos = require('./persistence/emprestimo_persistence')
 
 
 let prod4 = {
-    nome:"Produto 4"
+    nome:"Emprestimo 4"
 }
 
-cadastroProdutos.inserir(prod4)
+cadastroEmprestimos.inserir(prod4)
 
 let prod5 = {
-    nome:"Produto 5"
+    nome:"Emprestimo 5"
 }
 
-cadastroProdutos.inserir(prod5)
+cadastroEmprestimos.inserir(prod5)
 
 
-console.log("Listar",cadastroProdutos.listar())
+console.log("Listar",cadastroEmprestimos.listar())
 
 try {
-    console.log("Buscar Por ID=2", cadastroProdutos.buscarPorId(2))
+    console.log("Buscar Por ID=2", cadastroEmprestimos.buscarPorId(2))
 } catch(err) { 
     console.log("Erro", err);
 } 
 try {
-    console.log("Buscar Por ID=4", cadastroProdutos.buscarPorId(4))
+    console.log("Buscar Por ID=4", cadastroEmprestimos.buscarPorId(4))
 } catch(err) { 
     console.log("Erro", err);
 } 
 try {
-    console.log("Buscar Por ID=6", cadastroProdutos.buscarPorId(6))
+    console.log("Buscar Por ID=6", cadastroEmprestimos.buscarPorId(6))
 } catch(err) { 
     console.log("Erro", err);
 } 
 
-cadastroProdutos.atualizar(1, { preco: 11 })
+cadastroEmprestimos.atualizar(1, { preco: 11 })
 
-console.log("Listar",cadastroProdutos.listar())
+console.log("Listar",cadastroEmprestimos.listar())
 
 
